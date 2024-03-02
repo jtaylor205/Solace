bc9c35fb0079978d65294ea7389f786ff0fe9112
Alexander Klein <agklein04@outlook.com>
2024-03-01 22:07:11 -0500

    Bottom Tab Navigator Created

--- App.js
+++ App.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import { StatusBar, StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import MyTabs from './utils/Navigator.js'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Fortnite Roleplay Pokemon Toilet</Text>
-      <View style={styles.imageContainer}>
-        <Image source={require('./assets/frpt.jpg')} style={styles.image} resizeMode="contain" />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      // NavigationContainer component acts as the root container for all your navigation components.
+      <NavigationContainer> 
+        <MyTabs />
+      </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fBf',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  imageContainer: {
-    width: '80%', // Adjust container width as needed
-    height: 300, // Adjust container height as needed
-    overflow: 'hidden', // Ensure that the image is clipped at the bottom
-    marginBottom: 20,
-  },
-  image: {
-    flex: 1,
-    width: undefined,
-    height: undefined,
-  },
+
 });
